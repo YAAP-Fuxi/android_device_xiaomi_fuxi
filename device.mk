e441eccf0d7a426fc5fc34e16e8c97452f8cee8b
OodavidsinoO <david.sin@tuta.io>
2026-04-12 14:45:13 +0800

    fuxi: overlay: Add additional URLs for captive portal checks For regions that can not access Google's server directly

--- device.mk
+++ device.mk
@@ -36,7 +36,8 @@ PRODUCT_PACKAGES += \
     EuiccResFuxi \
     FrameworkResOverlayFuxi \
     SystemUIOverlayFuxi \
-    SettingsOverlayFuxi
+    SettingsOverlayFuxi \
+    NetworkStackResOverlayFuxi
 
 # PowerShare
 $(call soong_config_set,lineage_powershare,powershare_path,/sys/class/qcom-battery/reverse_chg_mode)
